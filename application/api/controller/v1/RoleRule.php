28a9b40ef80a8bafa102b7d5df894b83cd754a98
akgstudio <bumaya1024@gmail.com>
2025-01-04 22:42:26 +0800

    增加RoleRule和AdminRule

---
+++ application/api/controller/v1/RoleRule.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace app\api\controller\v1;
+
+use app\api\controller\Base;
+use app\common\model\RuleModel;
+use think\Controller;
+use think\Request;
+
+class RoleRule extends Base
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index(Request $request)
+    {
+        //
+        $db = new RuleModel();
+        $list = $db->where('status',1)->field('id,name,pid,url,img')->select();
+        return json(['code'=>0, 'msg'=>'获取规则信息成功', 'data'=>$list]);
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
---
+++ application/common/model/RoleRuleModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+class RoleRuleModel extends Model
+{
+    //
+    protected $table = "role_rule"
+}
